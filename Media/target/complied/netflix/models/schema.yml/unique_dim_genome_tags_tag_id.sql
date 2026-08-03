5a4d80c660bd75287b3cfe024d3479899fe256d3
varsha <varshapradhan2200@gmail.com>
2026-08-03 21:40:47 +0530

    Create unique_dim_genome_tags_tag_id.sql

---
+++ Media/target/complied/netflix/models/schema.yml/unique_dim_genome_tags_tag_id.sql
@@ -0,0 +1,13 @@
+
+    
+    
+
+select
+    tag_id as unique_field,
+    count(*) as n_records
+
+from MOVIELENS.DEV.dim_genome_tags
+where tag_id is not null
+group by tag_id
+having count(*) > 1
+
